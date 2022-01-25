2f533dcc84f5bef94785f9edb86b6587510c2435
opbrutal <93695693+opbrutal@users.noreply.github.com>
2022-01-25 23:13:10 +0530

    Update play.py

--- handlers/play.py
+++ handlers/play.py
@@ -234,6 +234,7 @@ async def play(_, message: Message):
                 try:
 
                     invitelink = await _.export_chat_invite_link(chid)
+                    
                     if invitelink.startswith("https://t.me/+"):
 
                         invitelink = invitelink.replace("https://t.me/+","https://t.me/joinchat/")
