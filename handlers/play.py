58b219a7c40165845fea726a69ed855420a1b901
Lucky boy ❤️ <94621036+Mrvk1703@users.noreply.github.com>
2022-02-01 13:18:09 +0530

    Update play.py

--- handlers/play.py
+++ handlers/play.py
@@ -156,12 +156,12 @@ async def generate_cover(requested_by, title, views, duration, thumbnail):
 
     font = ImageFont.truetype("etc/font.otf", 60)
 
-    draw.text((170, 590), f"Playing by Lucky", (0, 0, 0), font=font)  
-    draw.text((170, 550), f"Title: {title}", (0, 0, 0), font=font)
+    draw.text((150, 590), f"Playing by Lucky", (0, 0, 0), font=font)  
+    draw.text((150, 540), f"Title: {title}", (0, 0, 0), font=font)
 
     draw.text(
 
-        (170, 630),
+        (170, 640),
 
         f"Powered By: Lucky(@cute_boy701)",
 
